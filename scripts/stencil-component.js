5ad5f3368e8014b652eba07c1b6f20dc17ff57d9
Edward Silverton <e.silverton@gmail.com>
2019-05-19 12:29:46 +0100

    test stencil one

--- scripts/stencil-component.js
+++ scripts/stencil-component.js
@@ -1,6 +1,6 @@
 (function() {
-	var componentName = 'iiifgallery';
-    var scriptUri = 'node_modules/iiif-gallery/dist';  
+	var componentName = 'mycomponent';
+    var scriptUri = 'node_modules/@edsilv/stencil-one-test/dist/' + componentName;  
     var t = document.createElement('script');
     t.type = 'text/javascript';
     t.src = scriptUri +'/' + componentName + '.js';
